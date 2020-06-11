6585b1de1cb3dbc8a6570865ecfcb1046dbde158
antnf3 <antnf3@gmail.com>
2020-06-11 21:34:21 +0900

    상세정보 박스 fetch추가중

--- src/components/BoxDetail.tsx
+++ src/components/BoxDetail.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { faEdit } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
@@ -7,16 +7,39 @@ import TmpBoxOutline from "./layout/TmpBoxOutline";
 import TmpBoxList from "./layout/TmpBoxList";
 import LabelDetail from "./LabelDetail";
 import { connect } from "react-redux";
-import { JobState } from "../store";
+import { query } from "../store";
+
+const category = { COUPANG_ID: "쿠팡아이디" };
 
 function BoxDetail(props: any) {
-  const { data } = props;
+  const { data, onFetch } = props;
   console.log(data);
 
   const data1 = [
     ["adfewrwer", "afdf3424", "dsfs254234", "dsfs24234"],
     ["234dsfsdf", "23423423", "342423"],
   ];
+
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const params = {
+        mapFile: "coupangInfo.search_coupangid tistory.search_token",
+        inData: { USER_ID: "k947114585", BLOG_TYPE: "01" },
+      };
+
+      const res = await onFetch(params);
+      if (res.type === "QUERY_SUCCESS") {
+        console.log(res.payload.data);
+        setListData(() => {
+          return res.payload.data;
+        });
+      }
+    };
+    search();
+  }, [onFetch]);
+
   return (
     <Fragment>
       {data1.map((data, i) => (
@@ -47,4 +70,8 @@ function mapStateToProps(state: any, ownProps: any) {
   );
   return { data: oneJob };
 }
-export default connect(mapStateToProps)(BoxDetail);
+
+function mapDispatchToProps(dispatch: any) {
+  return { onFetch: (params: any) => dispatch(query(params)) };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BoxDetail);
