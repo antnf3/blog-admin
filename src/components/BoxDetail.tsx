03aa388854720a9029877c630784fada56d9f915
antnf3 <antnf3@gmail.com>
2020-06-13 13:10:26 +0900

    작업등록 수정

--- src/components/BoxDetail.tsx
+++ src/components/BoxDetail.tsx
@@ -12,66 +12,29 @@ import { query } from "../store";
 const category = { COUPANG_ID: "쿠팡아이디" };
 
 function BoxDetail(props: any) {
-  const { data, onFetch } = props;
-  console.log(data);
+  const { list } = props;
 
-  const data1 = [
-    ["adfewrwer", "afdf3424", "dsfs254234", "dsfs24234"],
-    ["234dsfsdf", "23423423", "342423"],
-  ];
-
-  const [listData, setListData] = useState([]);
-
-  useEffect(() => {
-    const search = async () => {
-      const params = {
-        mapFile: "coupangInfo.search_coupangid tistory.search_token",
-        inData: { USER_ID: "k947114585", BLOG_TYPE: "01" },
-      };
-
-      const res = await onFetch(params);
-      if (res.type === "QUERY_SUCCESS") {
-        console.log(res.payload.data);
-        setListData(() => {
-          return res.payload.data;
-        });
-      }
-    };
-    search();
-  }, [onFetch]);
+  console.log(list);
 
   return (
     <Fragment>
-      {data1.map((data, i) => (
-        <Fragment key={i}>
-          <TmpBoxOutline>
-            <TmpBoxHeader title={"아이디"}>
-              <Link id="navLink3" to="/12364">
-                <FontAwesomeIcon icon={faEdit} />
-              </Link>
-            </TmpBoxHeader>
+      <Fragment>
+        <TmpBoxOutline>
+          <TmpBoxHeader title={list[0].title}>
+            <Link id="navLink3" to="/12364">
+              <FontAwesomeIcon icon={faEdit} />
+            </Link>
+          </TmpBoxHeader>
 
-            <TmpBoxList bgColor={"#f1f3f5"}>
-              {data.map((id: any) => (
-                <LabelDetail key={id} value={id} />
-              ))}
-            </TmpBoxList>
-          </TmpBoxOutline>
-        </Fragment>
-      ))}
+          <TmpBoxList bgColor={"#f1f3f5"}>
+            {list[0].data.map((id: any) => (
+              <LabelDetail key={id.COUPANG_ID} value={id.COUPANG_ID} />
+            ))}
+          </TmpBoxList>
+        </TmpBoxOutline>
+      </Fragment>
     </Fragment>
   );
 }
 
-function mapStateToProps(state: any, ownProps: any) {
-  // console.log(state);
-  const oneJob = state?.arrList.data.filter(
-    (job: any) => job.JOB_ID === ownProps.id
-  );
-  return { data: oneJob };
-}
-
-function mapDispatchToProps(dispatch: any) {
-  return { onFetch: (params: any) => dispatch(query(params)) };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(BoxDetail);
+export default BoxDetail;
--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Template from "../components/layout/Template";
 import BoxDetail from "../components/BoxDetail";
 import { connect } from "react-redux";
-import { JobState } from "../store";
+import { JobState, query } from "../store";
 import { RouteComponentProps } from "react-router-dom";
 
 interface DetailProps extends RouteComponentProps<any> {
   jobId: string;
+  onFetch: any;
 }
 
-function Detail({ jobId }: DetailProps) {
+function Detail({ jobId, onFetch }: DetailProps) {
+  const [list, setList] = useState<any>([]);
+  useEffect(() => {
+    const search = async () => {
+      const params = {
+        mapFile: "coupangInfo.search_coupangid tistory.search_token",
+        inData: { USER_ID: "k947114585", BLOG_TYPE: "01" },
+      };
+
+      const res = await onFetch(params);
+      if (res.type === "QUERY_SUCCESS") {
+        console.log(res.payload.data);
+        setList([{ title: "쿠팡아이디", data: res.payload.data[0] }]);
+      }
+    };
+    search();
+  }, [onFetch]);
+
+  if (list.length === 0) {
+    return null;
+  }
   return (
     <Template title={"작업등록"}>
-      <BoxDetail id={jobId}></BoxDetail>
+      <BoxDetail id={jobId} list={list}></BoxDetail>
     </Template>
   );
 }
 
 function mapStateToProps(state: JobState[], ownProps: any) {
+  console.log(state);
   return { jobId: ownProps.match.params.id };
 }
 
-export default connect(mapStateToProps)(Detail);
+function mapDispatchToProps(dispatch: any) {
+  return { onFetch: (params: any) => dispatch(query(params)) };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
